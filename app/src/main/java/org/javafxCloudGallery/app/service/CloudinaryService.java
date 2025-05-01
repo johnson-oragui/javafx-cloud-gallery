43e8fffe51ed3268790b133215de416ec7a72896
johnson-oragui <johnson.oragui@gmail.com>
2025-05-01 17:35:15 +0100

    feat: imported and used MyConfig for env variables

--- app/src/main/java/org/javafxCloudGallery/app/service/CloudinaryService.java
+++ app/src/main/java/org/javafxCloudGallery/app/service/CloudinaryService.java
@@ -3,23 +3,19 @@ package org.javafxCloudGallery.app.service;
 import java.io.IOException;
 import java.util.Map;
 
+import org.javafxCloudGallery.utilities.MyConfig;
+
 import com.cloudinary.*;
 import com.cloudinary.utils.ObjectUtils;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class CloudinaryService {
   private final Cloudinary cloudinary;
-  private static final Dotenv dotenv = Dotenv.load();
-  private static final String CLOUDINARY_CLOUD_NAME = dotenv.get("CLOUDINARY_CLOUD_NAME");
-  private static final String CLOUDINARY_API_KEY = dotenv.get("CLOUDINARY_API_KEY");
-  private static final String CLOUDINARY_API_SECRET = dotenv.get("CLOUDINARY_API_SECRET");
 
   public CloudinaryService() {
     cloudinary = new Cloudinary(ObjectUtils.asMap(
-        "cloud_name", CLOUDINARY_CLOUD_NAME,
-        "api_key", CLOUDINARY_API_KEY,
-        "api_secret", CLOUDINARY_API_SECRET,
+        "cloud_name", MyConfig.CLOUDINARY_CLOUD_NAME,
+        "api_key", MyConfig.CLOUDINARY_API_KEY,
+        "api_secret", MyConfig.CLOUDINARY_API_SECRET,
         "secure", true));
   }
 
--- database/src/main/java/org/javafcCloudGallery/database/DatabaseManager.java
+++ database/src/main/java/org/javafcCloudGallery/database/DatabaseManager.java
@@ -1,21 +1,15 @@
 package org.javafcCloudGallery.database;
 
+import org.javafxCloudGallery.utilities.MyConfig;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class DatabaseManager {
-  private static final Dotenv dotenv = Dotenv.load();
-  private static final String URL = dotenv.get("DB_URL");
-  private static final String USER = dotenv.get("POSTGRES_USER");
-  private static final String PASSWORD = dotenv.get("POSTGRES_PASSWORD");
 
   public static Connection getConnection() throws SQLException, Exception {
-    if (URL == null || USER == null || PASSWORD == null) {
-      throw new Exception("DATABASE URL, PASSWORD, USER missing in config");
-    }
-    return DriverManager.getConnection(URL, USER, PASSWORD);
+
+    return DriverManager.getConnection(MyConfig.DB_URL, MyConfig.DB_USER, MyConfig.DB_PASSWORD);
   }
 }
