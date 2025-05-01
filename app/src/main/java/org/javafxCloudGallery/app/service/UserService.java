0e73ec5febb14a267ba21a78e45d77f1bd943411
johnson-oragui <johnson.oragui@gmail.com>
2025-05-01 16:57:14 +0100

    feat: added UserService

---
+++ app/src/main/java/org/javafxCloudGallery/app/service/UserService.java
@@ -0,0 +1,48 @@
+package org.javafxCloudGallery.app.service;
+
+import java.util.Optional;
+
+import org.javafcCloudGallery.database.models.User;
+import org.javafcCloudGallery.database.repository.UserRepository;
+import org.javafxCloudGallery.utilities.PasswordUtil;
+import org.javafxCloudGallery.utilities.Tuple;
+import org.javafxCloudGallery.utilities.ValidationUtil;
+
+public class UserService {
+  public static void registerUser(String username, String email, String password, String confirmPassword)
+      throws Exception {
+    if (username == null || email == null || password == null) {
+      throw new Exception("username, email, and password must be provided");
+    }
+    if (!password.equals(confirmPassword)) {
+      throw new Exception("password and confirm password must match");
+    }
+
+    Tuple<String, String, String> tuple = ValidationUtil.validator(username, email, password);
+
+    String hashedPassword = PasswordUtil.hashPassword(tuple.getPassword());
+
+    Boolean isCreated = UserRepository.createNewUser(tuple.getUsername(), hashedPassword, tuple.getEmail());
+
+    if (!isCreated) {
+      throw new Exception("Something went wrong. User could not be registered");
+    }
+
+  }
+
+  public static User authenticate(String usernameOrEMail, String password) throws Exception {
+    if (usernameOrEMail.trim().length() < 3) {
+      throw new Exception("username or email must have length upto three(3)");
+    }
+    Optional<User> foundUser = UserRepository.findUserByEmailOrUsername(usernameOrEMail);
+    if (foundUser.isPresent()) {
+      User user = foundUser.get();
+      Boolean isPasswordValid = PasswordUtil.verifyPassword(user.getPassword(), password);
+      if (isPasswordValid) {
+        return user;
+      }
+    }
+    throw new Exception("invalid credentials");
+
+  }
+}
