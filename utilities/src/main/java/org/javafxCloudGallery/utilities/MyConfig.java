9cc5de0757f4523cd2ae67a3034bff4f774855fd
johnson-oragui <johnson.oragui@gmail.com>
2025-05-01 17:34:08 +0100

    feat: added MyConfig to hold all env variables

---
+++ utilities/src/main/java/org/javafxCloudGallery/utilities/MyConfig.java
@@ -0,0 +1,15 @@
+package org.javafxCloudGallery.utilities;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class MyConfig {
+  public static final Dotenv dotenv = Dotenv.load();
+  public static final String DB_URL = dotenv.get("DB_URL");
+  public static final String DB_USER = dotenv.get("POSTGRES_USER");
+  public static final String DB_PASSWORD = dotenv.get("POSTGRES_PASSWORD");
+
+  public static final String CLOUDINARY_CLOUD_NAME = dotenv.get("CLOUDINARY_CLOUD_NAME");
+  public static final String CLOUDINARY_API_KEY = dotenv.get("CLOUDINARY_API_KEY");
+  public static final String CLOUDINARY_API_SECRET = dotenv.get("CLOUDINARY_API_SECRET");
+
+}
