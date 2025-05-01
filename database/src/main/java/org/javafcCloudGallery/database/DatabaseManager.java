abe899e6a47ec12b7b4b844fec8a15f320a977ff
johnson-oragui <johnson.oragui@gmail.com>
2025-05-01 16:47:02 +0100

    feat: updated DatabaseManager with dotenv

--- database/src/main/java/org/javafcCloudGallery/database/DatabaseManager.java
+++ database/src/main/java/org/javafcCloudGallery/database/DatabaseManager.java
@@ -4,12 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseManager {
-  private static final String URL = System.getenv("DB_URL");
-  private static final String USER = System.getenv("POSTGRES_USER");
-  private static final String PASSWORD = System.getenv("POSTGRES_PASSWORD");
+import io.github.cdimascio.dotenv.Dotenv;
 
-  public static Connection getConnection() throws SQLException {
+public class DatabaseManager {
+  private static final Dotenv dotenv = Dotenv.load();
+  private static final String URL = dotenv.get("DB_URL");
+  private static final String USER = dotenv.get("POSTGRES_USER");
+  private static final String PASSWORD = dotenv.get("POSTGRES_PASSWORD");
+
+  public static Connection getConnection() throws SQLException, Exception {
+    if (URL == null || USER == null || PASSWORD == null) {
+      throw new Exception("DATABASE URL, PASSWORD, USER missing in config");
+    }
     return DriverManager.getConnection(URL, USER, PASSWORD);
   }
 }
